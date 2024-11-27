cf2cf70e84060c39fe6181b729b07b228385e58a
Lendra <lendrjhh@gmail.com>
2024-11-28 06:41:11 +0700

    Add files via upload

---
+++ web(2).sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 27, 2024 at 10:39 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `nomor_pesanan` int NOT NULL,
+  `user_id` int NOT NULL,
+  `item` varchar(100) NOT NULL,
+  `harga` int NOT NULL,
+  `informasi` varchar(255) NOT NULL,
+  `pembayaran` varchar(255) NOT NULL,
+  `tanggal_pesanan` datetime DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('proses','berhasil','gagal') DEFAULT 'proses'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`nomor_pesanan`, `user_id`, `item`, `harga`, `informasi`, `pembayaran`, `tanggal_pesanan`, `status`) VALUES
+(93, 1, 'LinkAja 10rb', 11000, 'Nomor Telepon: 085745735072', 'QRIS', '2024-11-24 11:50:52', 'berhasil'),
+(94, 3, 'LinkAja 5rb', 6000, 'Nomor Telepon: 085852935747', 'QRIS', '2024-11-24 11:52:01', 'berhasil'),
+(95, 3, 'DANA 20rb', 21000, 'Nomor Telepon: 085852935747', 'QRIS', '2024-11-24 11:54:01', 'berhasil'),
+(96, 1, 'DANA 100rb', 101000, 'Nomor Telepon: 085745735072', 'QRIS', '2024-11-24 12:44:10', 'proses'),
+(97, 1, 'OVO 20rb', 21000, 'Nomor Telepon: dimas anjay mabar professional', 'QRIS', '2024-11-24 12:46:43', 'proses'),
+(98, 3, 'MLBB 199 DM', 11000, 'User ID: 123123 | Server ID: 13123', 'QRIS', '2024-11-25 00:13:16', 'proses'),
+(99, 3, 'MLBB 59 DM', 6000, 'User ID: asdasd | Server ID: asdasd', 'QRIS', '2024-11-25 00:14:27', 'gagal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `date`, `last_login`) VALUES
+(1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', '2024-11-23 06:12:32', '2024-11-27 20:15:50'),
+(2, 'user', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'user', '2024-11-23 06:12:44', '2024-11-27 20:15:50'),
+(3, 'lendra', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'user', '2024-11-23 06:33:19', '2024-11-27 20:15:50');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`nomor_pesanan`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `nomor_pesanan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
